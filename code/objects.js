e193ab5ce7aa7490237e0db90b77d388f1fc1dfd
Sandra <sandramtzd@hotmail.com>
2023-08-22 22:38:52 +0100

    adding information to an object

--- code/objects.js
+++ code/objects.js
@@ -64,13 +64,15 @@ parrot = {
   breed: "African Grey Parrot"
 };
 
-console.log(parrot);
+//console.log(parrot);
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
  */
 
-// console.log(tortoise);
+tortoise.favouriteFood = "dandelion";
+
+console.log(tortoise);
 
 /**
  * Q7. What's the combined age of all the pets?
